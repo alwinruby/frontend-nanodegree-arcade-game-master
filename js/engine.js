7bd72360694763b5993325b934a007657a4bb457
asolanky <alwinsolanky@gmail.com>
2019-02-05 23:15:53 +0000

    muck about

--- js/engine.js
+++ js/engine.js
@@ -113,7 +113,8 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                //'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/rock.png'
             ],
             numRows = 6,
             numCols = 5,
@@ -171,7 +172,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/rock.png'
     ]);
     Resources.onReady(init);
 
