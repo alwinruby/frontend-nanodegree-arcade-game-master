3a6610cdd13eaf609348c41d6ffb190f87ca6df5
asolanky <alwinsolanky@gmail.com>
2019-02-05 20:56:54 +0000

    enemies to move across the screen

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y = 0;
+    this.x = Enemy.offScreenLocation;
+    this.y = 60;
+    this.speed = 200;
 };
+
+Enemy.offScreenLocation = -100;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,6 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // debugger;
+    this.x = this.x + this.speed * dt;
+
+    if (this.x > ctx.canvas.width) {
+        this.x = Enemy.offScreenLocation;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -27,7 +36,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 function Player() {
-    this.sprite = 'images/char-pink-girl.png';
+    this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 380;
 }
@@ -43,9 +52,9 @@ Player.prototype.render = function () {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 var allEnemies = [new Enemy(), new Enemy()];
 var player = new Player();
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
